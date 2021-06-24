88def4b7a7dbbb1b28d5878997ed4afc6d6082f6
Vlado <vladimir.kokic@gmail.com>
2021-06-24 23:56:03 +0200

    Update bookReducer.js

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -2,6 +2,7 @@ const bookReducer = (state = "", action) => {
   switch (action.type) {
     case "SELECTION":
       return {
+        ...state,
         title: action.title,
         cost: action.data,
       };
